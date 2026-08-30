fc21c8c640abbf24f6685c2e5f2f640e777ab10b
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-30 15:56:29 +0300

    add refetchOnMount and transfer components mutations

---
+++ lib/api.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import type {Note, NewNote} from "@/types/note"
+
+
+const BASE_URL = "https://notehub-public.goit.study/api/notes";
+const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { Authorization: `Bearer ${TOKEN}` },
+});
+
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+interface FetchNotesParams {
+  page: number;
+  perPage: number;
+  search?: string;
+}
+
+
+export const fetchNotes = async (params: FetchNotesParams): Promise<FetchNotesResponse> => {
+  const response = await api.get<FetchNotesResponse>("", { params });
+  return response.data;
+};
+
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const response = await api.get<Note>(`/${id}`);
+  return response.data;
+};
+
+export const createNote = async (note: NewNote): Promise<Note> => {
+  const response = await api.post<Note>("", note);
+  return response.data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response = await api.delete<Note>(`/${id}`);
+  return response.data;
+};
+
+
